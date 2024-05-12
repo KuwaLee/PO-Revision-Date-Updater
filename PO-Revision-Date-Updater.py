1538f94dfeb4c784159506d50b1cd1ab48e3d71d
Kuwa Lee <kuwalee1069@gmail.com>
2024-05-13 00:34:36 +0800

    Update PO-Revision-Date-Updater.py

--- PO-Revision-Date-Updater.py
+++ PO-Revision-Date-Updater.py
@@ -10,9 +10,9 @@ def update_po_revision_date(file_path, timezone='UTC'):
     # Get the current time stamp and convert it to the specified time zone.
     tz = pytz.timezone(timezone)
     now = datetime.now(tz)
-    formatted_date = now.strftime('PO-Revision-Date: %Y-%m-%d %H:%M%z\\\\n')
+    formatted_date = now.strftime('PO-Revision-Date: %Y-%m-%d %H:%M%z\\\\')
     # Update "PO-Revision-Date" time stamp.
-    new_content = re.sub(r'PO-Revision-Date:.+\\n', formatted_date, content)
+    new_content = re.sub(r'PO-Revision-Date:.+\\', formatted_date, content)
 
     # Write back to file
     
